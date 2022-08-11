219504b1622298704593510af602b0af1f1d447d
ducminhha <tranducminh_ha@yahoo.com>
2022-08-12 04:26:52 +1200

    Change back to default tree

--- src/index.tsx
+++ src/index.tsx
@@ -14,6 +14,6 @@ const root = ReactDOM.createRoot(
 );
 root.render(
     <React.StrictMode>
-        <Tree people={familyTree2} />
+        <Tree people={familyTree} />
     </React.StrictMode>,
 );
